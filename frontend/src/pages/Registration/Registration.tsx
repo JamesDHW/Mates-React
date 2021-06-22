8b5b1542a5dfa42a0d60533ee33aa9b8a0d9fce1
JamesDHW <JamesHaworthWheatman@gmail.com>
2021-06-22 23:01:07 +0100

    remove duplicate styles

--- frontend/src/pages/Registration/Registration.tsx
+++ frontend/src/pages/Registration/Registration.tsx
@@ -1,17 +1,14 @@
 import React, { FC, useState } from 'react'
-import { View, Image, StyleSheet } from 'react-native'
 import FormInput from '../../components/FormInput'
 import FormButton from '../../components/FormButton/FormButton'
+import { Container, Logo } from '../Login/Login.style'
 
 export const Registration: FC = () => {
   const [email, setEmail] = useState<string>()
   const [password, setPassword] = useState<string>()
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../../../assets/friends.png')}
-      />
+    <Container>
+      <Logo source={require('../../../assets/friends.png')} />
       <FormInput
         labelValue={email}
         onChangeText={(userEmail: string) => setEmail(userEmail)}
@@ -47,47 +44,6 @@ export const Registration: FC = () => {
         autoCorrect={false}
       />
       <FormButton buttonTitle="Register" onPress={() => null} />
-    </View>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    paddingTop: 50,
-  },
-  logo: {
-    height: 200,
-    width: 300,
-    marginVertical: 20,
-    resizeMode: 'cover',
-  },
-  text: {
-    fontSize: 28,
-    marginBottom: 10,
-    fontWeight: '500',
-    color: '#051d5f',
-  },
-  textRegister: {
-    fontSize: 15,
-    marginVertical: 10,
-    fontWeight: '500',
-    color: '#abb8c3',
-  },
-  navButton: {
-    marginTop: 15,
-  },
-  forgotButton: {
-    marginVertical: 15,
-  },
-  newAccountButton: {
-    marginBottom: 36,
-  },
-  navButtonText: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: '#2e64e5',
-  },
-})
